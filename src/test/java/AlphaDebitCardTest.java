7cb9cb83131014177c15484a4619442117f25b12
Svetlana Gurenko <svetlashaaaa@gmail.com>
2023-06-01 15:49:22 +0300

    Multiple Fixes

--- src/test/java/AlphaDebitCardTest.java
+++ src/test/java/AlphaDebitCardTest.java
@@ -1,5 +1,6 @@
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.$;
@@ -7,9 +8,14 @@ import static com.codeborne.selenide.Selenide.open;
 
 
 public class AlphaDebitCardTest {
+    @BeforeEach
+    public void setup() {
+        open("http://localhost:9999");
+
+    }
     @Test
     void shouldSumbitRequest() {
-        open("http://localhost:9999");
+
         SelenideElement request = $(".form");
         request.$("[data-test-id=name] input").setValue("Сильнов Дима");
         request.$("[data-test-id=phone] input").setValue("+79999999999");
@@ -20,34 +26,52 @@ public class AlphaDebitCardTest {
 
     @Test
     void notValidDataInName() {
-        open("http://localhost:9999");
+
         SelenideElement request = $(".form");
         request.$("[data-test-id=name] input").setValue("Silnov Dima");
         request.$("[data-test-id=phone] input").setValue("+79999999999");
         request.$("[data-test-id=agreement]").click();
         request.$(".button").click();
-        $(".input_invalid .input__sub").shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 
     @Test
     void notValidDataInPhone() {
-        open("http://localhost:9999");
+
         SelenideElement request = $(".form");
         request.$("[data-test-id=name] input").setValue("Сильнов Дима");
         request.$("[data-test-id=phone] input").setValue("QA");
         request.$("[data-test-id=agreement]").click();
         request.$(".button").click();
-        $(".input_invalid .input__sub").shouldHave(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 
     @Test
     void notCheckbox() {
-        open("http://localhost:9999");
+
         SelenideElement request = $(".form");
         request.$("[data-test-id=name] input").setValue("Сильнов Дима");
         request.$("[data-test-id=phone] input").setValue("+79999999999");
         //не установили чекбокс, но пытаемся создать заявку
         request.$(".button").click();
         $(".input_invalid .checkbox__text").shouldHave(Condition.exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
+    }
+    @Test
+    void emptyFieldName() {
+        SelenideElement request = $(".form");
+        // Оставляем пустым поле ввода имени и фамилии
+        request.$("[data-test-id=phone] input").setValue("QA");
+        request.$("[data-test-id=agreement]").click();
+        request.$(".button").click();
+        $(".input_invalid .input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
+    }
+    @Test
+    void emptyFieldPhone() {
+        SelenideElement request = $(".form");
+        request.$("[data-test-id=name] input").setValue("Сильнов Дима");
+        //Оставляем пустым поле ввода номера телефона
+        request.$("[data-test-id=agreement]").click();
+        request.$(".button").click();
+        $(".input_invalid .input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
     }
 }
